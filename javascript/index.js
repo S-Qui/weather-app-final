9d2a6abc70c456d230ab3f18227968f2c7d3600b
S-Qui <147270652+S-Qui@users.noreply.github.com>
2023-12-15 15:43:21 +0300

    added forecast cards

---
+++ javascript/index.js
@@ -0,0 +1,73 @@
+//Change temperature based on the city: working
+function currentTemperature(response) {
+  let cityTemp = Math.round(response.data.temperature.current);
+  let tempElement = document.querySelector("#current-temp");
+  tempElement.innerHTML = cityTemp;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+ 
+ let iconElement = document.querySelector("#icon");
+ iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+ 
+}
+//Change h1 city to search result: working
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  
+
+  let apiKey = "e84f68cofe23a7b324t37b5eb50612a2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInputElement.value}`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+
+// Update to local time: working
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
